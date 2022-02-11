531fd40f3f20cf4da3a5eca564d3554111176181
Julius Härtl <jus@bitgrid.net>
2022-02-11 11:42:45 +0100

    Move to IBootstrap
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -25,86 +25,105 @@ declare(strict_types=1);
 
 namespace OCA\UserOIDC\AppInfo;
 
+use Exception;
+use OC_App;
+use OC_User;
 use OCA\UserOIDC\Db\ProviderMapper;
 use OCA\UserOIDC\Service\ID4MeService;
 use OCA\UserOIDC\Service\SettingsService;
 use OCA\UserOIDC\User\Backend;
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\IL10N;
 use OCP\IRequest;
 use OCP\IURLGenerator;
 use OCP\IUserManager;
 use OCP\IUserSession;
+use Throwable;
 
-class Application extends App {
+class Application extends App implements IBootstrap {
 	public const APP_ID = 'user_oidc';
 	public const OIDC_API_REQ_HEADER = 'Authorization';
+
+	private $cachedProviders;
 
 	public function __construct(array $urlParams = []) {
 		parent::__construct(self::APP_ID, $urlParams);
 	}
 
-	public function register() {
-		/** @var IUserSession $userSession */
-		$userSession = $this->getContainer()->query(IUserSession::class);
-
+	public function register(IRegistrationContext $context): void {
 		/** @var IUserManager $userManager */
-		$userManager = $this->getContainer()->query(IUserManager::class);
+		$userManager = $this->getContainer()->get(IUserManager::class);
 
 		/* Register our own user backend */
-		$backend = $this->getContainer()->query(Backend::class);
+		$backend = $this->getContainer()->get(Backend::class);
 		$userManager->registerBackend($backend);
-		\OC_User::useBackend($backend);
+		OC_User::useBackend($backend);
+	}
 
-		if (!$userSession->isLoggedIn()) {
-
-			/** @var IURLGenerator $urlGenerator */
-			$urlGenerator = $this->getContainer()->query(IURLGenerator::class);
-
-			/** @var ProviderMapper $providerMapper */
-			$providerMapper = $this->getContainer()->query(ProviderMapper::class);
-			$providers = $providerMapper->getProviders();
-
-			/** @var IL10N $l10n */
-			$l10n = $this->getContainer()->query(IL10N::class);
-			/** @var IRequest $request */
-			$request = $this->getContainer()->query(IRequest::class);
-			/** @var SettingsService $settings */
-			$settings = $this->getContainer()->query(SettingsService::class);
-
-			$redirectUrl = $request->getParam('redirect_url');
-
-			// Handle immediate redirect to the oidc provider if just one is configured and no other backens are allowed
-			$isDefaultLogin = false;
-			try {
-				$isDefaultLogin = $request->getPathInfo() === '/login' && $request->getParam('direct') !== '1';
-			} catch (\Exception $e) {
-				// in case any errors happen when checkinf for the path do not apply redirect logic as it is only needed for the login
-			}
-			if ($isDefaultLogin && !$settings->getAllowMultipleUserBackEnds() && count($providers) === 1) {
-				$targetUrl = $urlGenerator->linkToRoute(self::APP_ID . '.login.login', [
-					'providerId' => $providers[0]->getId(),
-					'redirectUrl' => $redirectUrl
-				]);
-				header('Location: ' . $targetUrl);
-				exit();
-			}
-
-			foreach ($providers as $provider) {
-				\OC_App::registerLogIn([
-					'name' => $l10n->t('Login with %1s', [$provider->getIdentifier()]),
-					'href' => $urlGenerator->linkToRoute(self::APP_ID . '.login.login', ['providerId' => $provider->getId(), 'redirectUrl' => $redirectUrl]),
-				]);
-			}
-
-			/** @var ID4MeService $id4meService */
-			$id4meService = $this->getContainer()->query(ID4MeService::class);
-			if ($id4meService->getID4ME()) {
-				\OC_App::registerLogIn([
-					'name' => 'ID4ME',
-					'href' => $urlGenerator->linkToRoute(self::APP_ID . '.id4me.login'),
-				]);
-			}
+	public function boot(IBootContext $context): void {
+		/** @var IUserSession $userSession */
+		$userSession = $this->getContainer()->get(IUserSession::class);
+		if ($userSession->isLoggedIn()) {
+			return;
 		}
+
+		try {
+			$context->injectFn(\Closure::fromCallable([$this, 'registerRedirect']));
+			$context->injectFn(\Closure::fromCallable([$this, 'registerLogin']));
+		} catch (Throwable $e) {
+		}
+	}
+
+	private function registerRedirect(IRequest $request, IURLGenerator $urlGenerator, SettingsService $settings, ProviderMapper $providerMapper): void {
+		$providers = $this->getCachedProviders($providerMapper);
+		$redirectUrl = $request->getParam('redirect_url');
+
+		// Handle immediate redirect to the oidc provider if just one is configured and no other backends are allowed
+		$isDefaultLogin = false;
+		try {
+			$isDefaultLogin = $request->getPathInfo() === '/login' && $request->getParam('direct') !== '1';
+		} catch (Exception $e) {
+			// in case any errors happen when checking for the path do not apply redirect logic as it is only needed for the login
+		}
+		if ($isDefaultLogin && !$settings->getAllowMultipleUserBackEnds() && count($providers) === 1) {
+			$targetUrl = $urlGenerator->linkToRoute(self::APP_ID . '.login.login', [
+				'providerId' => $providers[0]->getId(),
+				'redirectUrl' => $redirectUrl
+			]);
+			header('Location: ' . $targetUrl);
+			exit();
+		}
+	}
+
+	private function registerLogin(IRequest $request, IL10N $l10n, IURLGenerator $urlGenerator, ProviderMapper $providerMapper): void {
+		$redirectUrl = $request->getParam('redirect_url');
+		$providers = $this->getCachedProviders($providerMapper);
+		foreach ($providers as $provider) {
+			// FIXME: Move to IAlternativeLogin but requires boot due to db connection
+			OC_App::registerLogIn([
+				'name' => $l10n->t('Login with %1s', [$provider->getIdentifier()]),
+				'href' => $urlGenerator->linkToRoute(self::APP_ID . '.login.login', ['providerId' => $provider->getId(), 'redirectUrl' => $redirectUrl]),
+			]);
+		}
+
+		/** @var ID4MeService $id4meService */
+		$id4meService = $this->getContainer()->get(ID4MeService::class);
+		if ($id4meService->getID4ME()) {
+			OC_App::registerLogIn([
+				'name' => 'ID4ME',
+				'href' => $urlGenerator->linkToRoute(self::APP_ID . '.id4me.login'),
+			]);
+		}
+	}
+
+	private function getCachedProviders(ProviderMapper $providerMapper): array {
+		if (!isset($this->cachedProviders)) {
+			$this->cachedProviders = $providerMapper->getProviders();
+		}
+
+		return $this->cachedProviders;
 	}
 }
