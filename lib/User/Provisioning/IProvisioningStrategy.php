78c1c4b46049b5b9b7a3c8bfb7f95d129f5912d4
agnisa <arne.gnisa@capgemini.com>
2022-09-09 12:20:56 +0200

    fix lint

--- lib/User/Provisioning/IProvisioningStrategy.php
+++ lib/User/Provisioning/IProvisioningStrategy.php
@@ -5,8 +5,7 @@ namespace OCA\UserOIDC\User\Provisioning;
 use OCA\UserOIDC\Db\Provider;
 use OCP\IUser;
 
-interface IProvisioningStrategy
-{
+interface IProvisioningStrategy {
 
 	/**
 	 * Defines a way to provision a user.
