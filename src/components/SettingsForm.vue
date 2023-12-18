9ea902dfb607978450ac7ea787cc7886bcc927a8
Florian Klinger <florian.klinger@nextcloud.com>
2023-12-18 13:51:23 +0100

    Rebase main and expandable/collapsable extra attributes in the frontend
    Signed-off-by: Florian Klinger <florian.klinger@nextcloud.com>

--- src/components/SettingsForm.vue
+++ src/components/SettingsForm.vue
@@ -106,126 +106,138 @@
 				placeholder="groups"
 				:disabled="!localProvider.settings.groupProvisioning">
 		</p>
-		<h3><b>{{ t('user_oidc', 'Profile attributes mapping') }}</b></h3>
-		<p>
-			<label for="mapping-displayName">{{ t('user_oidc', 'Display name mapping') }}</label>
-			<input id="mapping-displayName"
-				v-model="localProvider.settings.mappingDisplayName"
-				type="text"
-				placeholder="name">
-		</p>
-		<p>
-			<label for="mapping-gender">{{ t('user_oidc', 'Gender mapping') }}</label>
-			<input id="mapping-gender"
-				v-model="localProvider.settings.mappingGender"
-				type="text"
-				placeholder="gender">
-		</p>
-		<p>
-			<label for="mapping-email">{{ t('user_oidc', 'Email mapping') }}</label>
-			<input id="mapping-email"
-				v-model="localProvider.settings.mappingEmail"
-				type="text"
-				placeholder="email">
-		</p>
-		<p>
-			<label for="mapping-phone">{{ t('user_oidc', 'Phone mapping') }}</label>
-			<input id="mapping-phone"
-				v-model="localProvider.settings.mappingPhonenumber"
-				type="text"
-				placeholder="phone_number">
-		</p>
-		<p>
-			<label for="mapping-role">{{ t('user_oidc', 'Role/Title mapping') }}</label>
-			<input id="mapping-role"
-				v-model="localProvider.settings.mappingRole"
-				type="text"
-				placeholder="role">
-		</p>
-		<p>
-			<label for="mapping-street_address">{{ t('user_oidc', 'Street mapping') }}</label>
-			<input id="mapping-street_address"
-				v-model="localProvider.settings.mappingStreetaddress"
-				type="text"
-				placeholder="street_address">
-		</p>
-		<p>
-			<label for="mapping-postal_code">{{ t('user_oidc', 'Postal code mapping') }}</label>
-			<input id="mapping-postal_code"
-				v-model="localProvider.settings.mappingPostalcode"
-				type="text"
-				placeholder="postal_code">
-		</p>
-		<p>
-			<label for="mapping-locality">{{ t('user_oidc', 'Locality mapping') }}</label>
-			<input id="mapping-locality"
-				v-model="localProvider.settings.mappingLocality"
-				type="text"
-				placeholder="locality">
-		</p>
-		<p>
-			<label for="mapping-region">{{ t('user_oidc', 'Region mapping') }}</label>
-			<input id="mapping-region"
-				v-model="localProvider.settings.mappingRegion"
-				type="text"
-				placeholder="region">
-		</p>
-		<p>
-			<label for="mapping-country">{{ t('user_oidc', 'Country mapping') }}</label>
-			<input id="mapping-country"
-				v-model="localProvider.settings.mappingCountry"
-				type="text"
-				placeholder="country">
-		</p>
-		<p>
-			<label for="mapping-organisation">{{ t('user_oidc', 'Organisation mapping') }}</label>
-			<input id="mapping-organisation"
-				v-model="localProvider.settings.mappingOrganisation"
-				type="text"
-				placeholder="organisation">
-		</p>
-		<p>
-			<label for="mapping-website">{{ t('user_oidc', 'Website mapping') }}</label>
-			<input id="mapping-website"
-				v-model="localProvider.settings.mappingWebsite"
-				type="text"
-				placeholder="website">
-		</p>
-		<p>
-			<label for="mapping-avatar">{{ t('user_oidc', 'Avatar mapping') }}</label>
-			<input id="mapping-avatar"
-				v-model="localProvider.settings.mappingAvatar"
-				type="text"
-				placeholder="avatar">
-		</p>
-		<p>
-			<label for="mapping-biography">{{ t('user_oidc', 'Biography mapping') }}</label>
-			<input id="mapping-biography"
-				v-model="localProvider.settings.mappingBiography"
-				type="text"
-				placeholder="biography">
-		</p>
-		<p>
-			<label for="mapping-twitter">{{ t('user_oidc', 'Twitter mapping') }}</label>
-			<input id="mapping-twitter"
-				v-model="localProvider.settings.mappingTwitter"
-				type="text"
-				placeholder="twitter">
-		</p>
-		<p>
-			<label for="mapping-fediverse">{{ t('user_oidc', 'Fediverse/Nickname mapping') }}</label>
-			<input id="mapping-fediverse"
-				v-model="localProvider.settings.mappingFediverse"
-				type="text"
-				placeholder="fediverse">
-		</p>
-		<p>
-			<label for="mapping-headline">{{ t('user_oidc', 'Headline mapping') }}</label>
-			<input id="mapping-headline"
-				v-model="localProvider.settings.mappingHeadline"
-				type="text"
-				placeholder="headline">
-		</p>
+
+		<h3>
+			<NcButton type="secondary" @click="toggleProfileAttributes">
+				<template #icon>
+					<ExpandAllIcon v-if="!showProfileAttributes" :size="20" />
+					<CollapseAllIcon v-else :size="20" />
+				</template>
+				{{ t('user_oidc', 'Extra attributes mapping') }}
+			</NcButton>
+		</h3>
+
+		<div v-show="showProfileAttributes" class="profile-attributes">
+			<p>
+				<label for="mapping-displayName">{{ t('user_oidc', 'Display name mapping') }}</label>
+				<input id="mapping-displayName"
+					v-model="localProvider.settings.mappingDisplayName"
+					type="text"
+					placeholder="name">
+			</p>
+			<p>
+				<label for="mapping-gender">{{ t('user_oidc', 'Gender mapping') }}</label>
+				<input id="mapping-gender"
+					v-model="localProvider.settings.mappingGender"
+					type="text"
+					placeholder="gender">
+			</p>
+			<p>
+				<label for="mapping-email">{{ t('user_oidc', 'Email mapping') }}</label>
+				<input id="mapping-email"
+					v-model="localProvider.settings.mappingEmail"
+					type="text"
+					placeholder="email">
+			</p>
+			<p>
+				<label for="mapping-phone">{{ t('user_oidc', 'Phone mapping') }}</label>
+				<input id="mapping-phone"
+					v-model="localProvider.settings.mappingPhonenumber"
+					type="text"
+					placeholder="phone_number">
+			</p>
+			<p>
+				<label for="mapping-role">{{ t('user_oidc', 'Role/Title mapping') }}</label>
+				<input id="mapping-role"
+					v-model="localProvider.settings.mappingRole"
+					type="text"
+					placeholder="role">
+			</p>
+			<p>
+				<label for="mapping-street_address">{{ t('user_oidc', 'Street mapping') }}</label>
+				<input id="mapping-street_address"
+					v-model="localProvider.settings.mappingStreetaddress"
+					type="text"
+					placeholder="street_address">
+			</p>
+			<p>
+				<label for="mapping-postal_code">{{ t('user_oidc', 'Postal code mapping') }}</label>
+				<input id="mapping-postal_code"
+					v-model="localProvider.settings.mappingPostalcode"
+					type="text"
+					placeholder="postal_code">
+			</p>
+			<p>
+				<label for="mapping-locality">{{ t('user_oidc', 'Locality mapping') }}</label>
+				<input id="mapping-locality"
+					v-model="localProvider.settings.mappingLocality"
+					type="text"
+					placeholder="locality">
+			</p>
+			<p>
+				<label for="mapping-region">{{ t('user_oidc', 'Region mapping') }}</label>
+				<input id="mapping-region"
+					v-model="localProvider.settings.mappingRegion"
+					type="text"
+					placeholder="region">
+			</p>
+			<p>
+				<label for="mapping-country">{{ t('user_oidc', 'Country mapping') }}</label>
+				<input id="mapping-country"
+					v-model="localProvider.settings.mappingCountry"
+					type="text"
+					placeholder="country">
+			</p>
+			<p>
+				<label for="mapping-organisation">{{ t('user_oidc', 'Organisation mapping') }}</label>
+				<input id="mapping-organisation"
+					v-model="localProvider.settings.mappingOrganisation"
+					type="text"
+					placeholder="organisation">
+			</p>
+			<p>
+				<label for="mapping-website">{{ t('user_oidc', 'Website mapping') }}</label>
+				<input id="mapping-website"
+					v-model="localProvider.settings.mappingWebsite"
+					type="text"
+					placeholder="website">
+			</p>
+			<p>
+				<label for="mapping-avatar">{{ t('user_oidc', 'Avatar mapping') }}</label>
+				<input id="mapping-avatar"
+					v-model="localProvider.settings.mappingAvatar"
+					type="text"
+					placeholder="avatar">
+			</p>
+			<p>
+				<label for="mapping-biography">{{ t('user_oidc', 'Biography mapping') }}</label>
+				<input id="mapping-biography"
+					v-model="localProvider.settings.mappingBiography"
+					type="text"
+					placeholder="biography">
+			</p>
+			<p>
+				<label for="mapping-twitter">{{ t('user_oidc', 'Twitter mapping') }}</label>
+				<input id="mapping-twitter"
+					v-model="localProvider.settings.mappingTwitter"
+					type="text"
+					placeholder="twitter">
+			</p>
+			<p>
+				<label for="mapping-fediverse">{{ t('user_oidc', 'Fediverse/Nickname mapping') }}</label>
+				<input id="mapping-fediverse"
+					v-model="localProvider.settings.mappingFediverse"
+					type="text"
+					placeholder="fediverse">
+			</p>
+			<p>
+				<label for="mapping-headline">{{ t('user_oidc', 'Headline mapping') }}</label>
+				<input id="mapping-headline"
+					v-model="localProvider.settings.mappingHeadline"
+					type="text"
+					placeholder="headline">
+			</p>
+		</div>
 		<h3><b>{{ t('user_oidc', 'Authentication and Access Control Settings') }}</b></h3>
 		<NcCheckboxRadioSwitch :checked.sync="localProvider.settings.uniqueUid" wrapper-element="div">
 			{{ t('user_oidc', 'Use unique user id') }}
@@ -280,6 +292,8 @@
 <script>
 import AlertOutlineIcon from 'vue-material-design-icons/AlertOutline.vue'
 import CheckIcon from 'vue-material-design-icons/Check.vue'
+import ExpandAllIcon from 'vue-material-design-icons/ExpandAll.vue'
+import CollapseAllIcon from 'vue-material-design-icons/CollapseAll.vue'
 
 import NcCheckboxRadioSwitch from '@nextcloud/vue/dist/Components/NcCheckboxRadioSwitch.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
@@ -291,6 +305,8 @@ export default {
 		NcButton,
 		AlertOutlineIcon,
 		CheckIcon,
+		ExpandAllIcon,
+		CollapseAllIcon,
 	},
 	props: {
 		submitText: {
@@ -310,6 +326,7 @@ export default {
 		return {
 			localProvider: null,
 			maxIdentifierLength: 128,
+			showProfileAttributes: false,
 		}
 	},
 	computed: {
@@ -320,6 +337,11 @@ export default {
 	created() {
 		this.localProvider = this.provider
 		this.identifierInitiallySet = !!this.localProvider.identifier
+	},
+	methods: {
+		toggleProfileAttributes() {
+			this.showProfileAttributes = !this.showProfileAttributes
+		},
 	},
 }
 </script>
