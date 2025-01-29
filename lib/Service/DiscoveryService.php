e515c645b6bae84c1badf8c14729792e888403e7
Adphi <philippe.adrien.nousse@gmail.com>
2025-01-29 17:27:01 +0100

    fix '"kid" invalid, unable to lookup correct key' when keys are rotated
    Signed-off-by: Adphi <philippe.adrien.nousse@gmail.com>

--- lib/Service/DiscoveryService.php
+++ lib/Service/DiscoveryService.php
@@ -65,12 +65,13 @@ class DiscoveryService {
 	/**
 	 * @param Provider $provider
 	 * @param string $tokenToDecode This is used to potentially fix the missing alg in
+	 * @param bool $refresh
 	 * @return array
 	 * @throws \JsonException
 	 */
-	public function obtainJWK(Provider $provider, string $tokenToDecode): array {
+	public function obtainJWK(Provider $provider, string $tokenToDecode, bool $refresh = false): array {
 		$lastJwksRefresh = $this->providerService->getSetting($provider->getId(), ProviderService::SETTING_JWKS_CACHE_TIMESTAMP);
-		if ($lastJwksRefresh !== '' && (int)$lastJwksRefresh > time() - self::INVALIDATE_JWKS_CACHE_AFTER_SECONDS) {
+		if ($lastJwksRefresh !== '' && !$refresh && (int)$lastJwksRefresh > time() - self::INVALIDATE_JWKS_CACHE_AFTER_SECONDS) {
 			$rawJwks = $this->providerService->getSetting($provider->getId(), ProviderService::SETTING_JWKS_CACHE);
 			$rawJwks = json_decode($rawJwks, true);
 		} else {
