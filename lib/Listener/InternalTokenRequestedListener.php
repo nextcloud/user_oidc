f96a80656274633b3021d01f637d1a5ec05f35db
Julien Veyssier <julien-nc@posteo.net>
2025-03-12 14:33:04 +0100

    clarify events/listeners to get a token, cover 3 cases: token exchange, login token, internal token (from the 'oidc' app)
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

---
+++ lib/Listener/InternalTokenRequestedListener.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\UserOIDC\Listener;
+
+use OCA\UserOIDC\Event\InternalTokenRequestedEvent;
+use OCA\UserOIDC\Service\TokenService;
+use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventListener;
+use OCP\IUserSession;
+use Psr\Log\LoggerInterface;
+
+/**
+ * @implements IEventListener<InternalTokenRequestedEvent|Event>
+ */
+class InternalTokenRequestedListener implements IEventListener {
+
+	public function __construct(
+		private IUserSession $userSession,
+		private TokenService $tokenService,
+		private LoggerInterface $logger,
+	) {
+	}
+
+	public function handle(Event $event): void {
+		if (!$event instanceof InternalTokenRequestedEvent) {
+			return;
+		}
+
+		if (!$this->userSession->isLoggedIn()) {
+			return;
+		}
+
+		$targetAudience = $event->getTargetAudience();
+		$this->logger->debug('[InternalTokenRequestedListener] received request for audience: ' . $targetAudience);
+
+		// generate a token pair with the Oidc provider app
+		$userId = $this->userSession->getUser()?->getUID();
+		if ($userId !== null) {
+			$ncProviderToken = $this->tokenService->getTokenFromOidcProviderApp($userId, $targetAudience);
+			if ($ncProviderToken !== null) {
+				$event->setToken($ncProviderToken);
+			}
+		}
+	}
+}
