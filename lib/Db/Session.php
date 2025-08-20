c0322841a449d12a4ec102df2568a2e6fa9b1134
Julien Veyssier <julien-nc@posteo.net>
2025-08-20 16:47:13 +0200

    feat(revoke-token): react to a user session being revoked, call the IdP's end_session_endpoint to make sure it's not possible to login again from the invalidated browser session
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Db/Session.php
+++ lib/Db/Session.php
@@ -11,38 +11,49 @@ namespace OCA\UserOIDC\Db;
 use OCP\AppFramework\Db\Entity;
 
 /**
- * @method string getSid()
- * @method void setSid(string $sid)
- * @method string getSub()
- * @method void setSub(string $sub)
- * @method string getIss()
- * @method void setIss(string $iss)
- * @method int getAuthtokenId()
- * @method void setAuthtokenId(int $authtokenId)
- * @method string getNcSessionId()
- * @method void setNcSessionId(string $ncSessionId)
- * @method int getCreatedAt()
- * @method void setCreatedAt(int $createdAt)
+ * @method \string getSid()
+ * @method \void setSid(string $sid)
+ * @method \string getSub()
+ * @method \void setSub(string $sub)
+ * @method \string getIss()
+ * @method \void setIss(string $iss)
+ * @method \int getAuthtokenId()
+ * @method \void setAuthtokenId(int $authtokenId)
+ * @method \string getNcSessionId()
+ * @method \void setNcSessionId(string $ncSessionId)
+ * @method \int getCreatedAt()
+ * @method \void setCreatedAt(int $createdAt)
+ * @method \string|\null getIdToken()
+ * @method \void setIdToken(?string $idToken)
+ * @method \string|\null getUserId()
+ * @method \void setUserId(?string $userId)
+ * @method \int getProviderId()
+ * @method \void setProviderId(int $providerId)
+ * @method \int getIdpSessionClosed()
+ * @method \void setIdpSessionClosed(int $idpSessionClosed)
  */
 class Session extends Entity implements \JsonSerializable {
 
 	/** @var string */
 	protected $sid;
-
 	/** @var string */
 	protected $sub;
-
 	/** @var string */
 	protected $iss;
-
 	/** @var int */
 	protected $authtokenId;
-
 	/** @var string */
 	protected $ncSessionId;
-
 	/** @var int */
 	protected $createdAt;
+	/** @var ?string */
+	protected $idToken;
+	/** @var ?string */
+	protected $userId;
+	/** @var int */
+	protected $providerId;
+	/** @var int */
+	protected $idpSessionClosed;
 
 	public function __construct() {
 		$this->addType('sid', 'string');
@@ -51,18 +62,25 @@ class Session extends Entity implements \JsonSerializable {
 		$this->addType('authtoken_id', 'integer');
 		$this->addType('nc_session_id', 'string');
 		$this->addType('created_at', 'integer');
+		$this->addType('id_token', 'string');
+		$this->addType('user_id', 'string');
+		$this->addType('provider_id', 'integer');
+		$this->addType('idp_session_closed', 'integer');
 	}
 
 	#[\ReturnTypeWillChange]
 	public function jsonSerialize() {
 		return [
-			'id' => $this->id,
-			'sid' => $this->sid,
-			'sub' => $this->sub,
-			'iss' => $this->iss,
-			'authtoken_id' => $this->authtokenId,
-			'nc_session_id' => $this->ncSessionId,
-			'created_at' => $this->createdAt,
+			'id' => $this->getId(),
+			'sid' => $this->getSid(),
+			'sub' => $this->getSub(),
+			'iss' => $this->getIss(),
+			'authtoken_id' => $this->getAuthtokenId(),
+			'nc_session_id' => $this->getNcSessionId(),
+			'created_at' => $this->getCreatedAt(),
+			'user_id' => $this->getUserId(),
+			'provider_id' => $this->getProviderId(),
+			'idp_session_closed' => $this->getIdpSessionClosed() !== 0,
 		];
 	}
 }
