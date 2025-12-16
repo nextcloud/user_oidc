ecbece87debdbe91e6988c1ab0d1950d3593d7a3
Julien Veyssier <julien-nc@posteo.net>
2025-12-16 10:55:32 +0100

    feat(list-command): include client secret
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Command/ListProviders.php
+++ lib/Command/ListProviders.php
@@ -9,7 +9,9 @@ declare(strict_types=1);
 namespace OCA\UserOIDC\Command;
 
 use OC\Core\Command\Base;
+use OCA\UserOIDC\Db\ProviderMapper;
 use OCA\UserOIDC\Service\ProviderService;
+use OCP\Security\ICrypto;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,7 +19,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ListProviders extends Base {
 
 	public function __construct(
+		private ProviderMapper $providerMapper,
 		private ProviderService $providerService,
+		private ICrypto $crypto,
 	) {
 		parent::__construct();
 	}
@@ -25,7 +29,7 @@ class ListProviders extends Base {
 	protected function configure() {
 		$this
 			->setName('user_oidc:providers')
-			->setDescription('List all providers')
+			->setDescription('List all providers and print their configuration')
 			->addOption('sensitive', 's', InputOption::VALUE_NONE, 'Obfuscate sensitive values like the client ID and the discovery endpoint domain name');
 		$this->defaultOutputFormat = self::OUTPUT_FORMAT_JSON_PRETTY;
 		parent::configure();
@@ -33,19 +37,27 @@ class ListProviders extends Base {
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$outputFormat = $input->getOption('output') ?? 'json_pretty';
+		$sensitive = $input->getOption('sensitive');
 
-		$providersWithSettings = $this->providerService->getProvidersWithSettings();
-		if ($input->getOption('sensitive')) {
-			$providersWithSettings = array_map(function ($provider) {
-				$provider['clientId'] = '********';
+		$providers = $this->providerMapper->getProviders();
+
+		$providersWithSettings = array_map(function ($provider) use ($sensitive) {
+			$providerSettings = $this->providerService->getSettings($provider->getId());
+			$serializedProvider = $provider->jsonSerialize();
+			if ($sensitive) {
+				$serializedProvider['clientId'] = '********';
+				$serializedProvider['clientSecret'] = '********';
 				try {
-					$discoveryDomainName = parse_url($provider['discoveryEndpoint'], PHP_URL_HOST);
-					$provider['discoveryEndpoint'] = str_replace($discoveryDomainName, '********', $provider['discoveryEndpoint']);
+					$discoveryDomainName = parse_url($serializedProvider['discoveryEndpoint'], PHP_URL_HOST);
+					$serializedProvider['discoveryEndpoint'] = str_replace($discoveryDomainName, '********', $serializedProvider['discoveryEndpoint']);
 				} catch (\Exception|\Throwable) {
 				}
-				return $provider;
-			}, $providersWithSettings);
-		}
+			} else {
+				$serializedProvider['clientSecret'] = $this->crypto->decrypt($provider->getClientSecret());
+			}
+			return array_merge($serializedProvider, ['settings' => $providerSettings]);
+		}, $providers);
+
 		if ($outputFormat === 'json') {
 			foreach ($providersWithSettings as $provider) {
 				$output->writeln(json_encode($provider, JSON_THROW_ON_ERROR));
