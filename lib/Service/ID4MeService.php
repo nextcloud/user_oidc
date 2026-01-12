ca212e762e18a8bb0092c4db0d204a17ba004300
Julien Veyssier <julien-nc@posteo.net>
2026-01-12 13:25:55 +0100

    fix: adjustments to stable24
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Service/ID4MeService.php
+++ lib/Service/ID4MeService.php
@@ -26,15 +26,41 @@ declare(strict_types=1);
 namespace OCA\UserOIDC\Service;
 
 use OCA\UserOIDC\AppInfo\Application;
+use OCA\UserOIDC\Vendor\Firebase\JWT\JWK;
+use OCP\Http\Client\IClientService;
+use OCP\ICacheFactory;
 use OCP\IConfig;
+use Psr\Log\LoggerInterface;
 
 class ID4MeService {
 
 	/** @var IConfig */
 	private $config;
 
-	public function __construct(IConfig $config) {
+	/** @var \OCP\ICache */
+	private $cache;
+
+	/** @var \OCP\Http\Client\IClient */
+	private $client;
+
+	/** @var LoggerInterface */
+	private $logger;
+
+	/** @var DiscoveryService */
+	private $discoveryService;
+
+	public function __construct(
+		IConfig $config,
+		DiscoveryService $discoveryService,
+		IClientService $clientService,
+		ICacheFactory $cacheFactory,
+		LoggerInterface $logger
+	) {
 		$this->config = $config;
+		$this->cache = $cacheFactory->createDistributed('user_oidc');
+		$this->client = $clientService->newClient();
+		$this->logger = $logger;
+		$this->discoveryService = $discoveryService;
 	}
 
 	public function setID4ME(bool $enabled): void {
@@ -49,11 +75,11 @@ class ID4MeService {
 		$cacheKey = 'jwks-' . $jwkUri;
 		$cachedJwks = $this->cache->get($cacheKey);
 		if ($cachedJwks !== null && $useCache) {
-			$rawJwks = json_decode($cachedJwks, true, flags: JSON_THROW_ON_ERROR);
+			$rawJwks = json_decode($cachedJwks, true, 512, JSON_THROW_ON_ERROR);
 			$this->logger->debug('[ID4ME-obtainJWK] jwks cache content', ['jwks_cache' => $rawJwks]);
 		} else {
-			$responseBody = (string)$this->clientService->get($jwkUri);
-			$rawJwks = json_decode($responseBody, true, flags: JSON_THROW_ON_ERROR);
+			$responseBody = (string)$this->client->get($jwkUri)->getBody();
+			$rawJwks = json_decode($responseBody, true, 512, JSON_THROW_ON_ERROR);
 			$this->logger->debug('[ID4ME-obtainJWK] getting fresh jwks', ['jwks' => $rawJwks]);
 			$this->cache->set($cacheKey, $responseBody, DiscoveryService::INVALIDATE_JWKS_CACHE_AFTER_SECONDS);
 		}
